51a62aec6b9160a315f5976001da5d5a8f7e6374
Mateusz Krakowiak <mateusz.krakowiak.dev@gmail.com>
2021-09-12 11:56:37 +0200

    Show details of dishes in menu details

--- emenu/menu/serializers.py
+++ emenu/menu/serializers.py
@@ -1,12 +1,21 @@
 from emenu.menu.models import Menu, Dish
 from rest_framework import serializers
 
-class MenuSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Menu
-        fields = ['url', 'name', 'description', 'date_added', 'date_modified', 'dishes']
-
 class DishSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Dish
         fields = ['url', 'name', 'description', 'price', 'preparation_time', 'date_added', 'date_modified', 'is_vegan']
+
+
+class MenuDetailSerializer(serializers.HyperlinkedModelSerializer):
+    dishes = DishSerializer(many=True)
+
+    class Meta:
+        model = Menu
+        fields = ['url', 'name', 'description', 'date_added', 'date_modified', 'dishes']
+
+class MenuSerializer(serializers.HyperlinkedModelSerializer):
+    dishes = serializers.StringRelatedField(many=True)
+    class Meta:
+        model = Menu
+        fields = ['url', 'name', 'description', 'date_added', 'date_modified', 'dishes']
--- emenu/menu/views.py
+++ emenu/menu/views.py
@@ -1,6 +1,7 @@
 from emenu.menu.models import Dish, Menu
-from emenu.menu.serializers import DishSerializer, MenuSerializer
+from emenu.menu.serializers import DishSerializer, MenuSerializer, MenuDetailSerializer
 from rest_framework import permissions, viewsets
+from rest_framework.response import Response
 
 class MenuViewSet(viewsets.ModelViewSet):
     """
@@ -8,8 +9,12 @@ class MenuViewSet(viewsets.ModelViewSet):
     `update` and `destroy` actions.
     """
     queryset = Menu.objects.all()
-    serializer_class = MenuSerializer
+    serializer_class = MenuDetailSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def list(self, request):
+        serializer = MenuSerializer(self.queryset, many=True, context={'request': request})
+        return Response(serializer.data)
 
 class DishViewSet(viewsets.ModelViewSet):
     """
