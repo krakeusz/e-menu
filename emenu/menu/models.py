fd79102835869fd60e56b290af6f5b3103b49f19
Mateusz Krakowiak <mateusz.krakowiak.dev@gmail.com>
2021-09-12 10:10:12 +0200

    Create app template, add models

---
+++ emenu/menu/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.db.models import fields
+
+class Dish(models.Model):
+    name = fields.CharField(unique=True, max_length=200)
+    description = fields.TextField()
+    price = fields.DecimalField(max_digits=19, decimal_places=2)
+    preparation_time = fields.DurationField()
+    date_added = fields.DateTimeField(auto_now_add=True)
+    date_modified = fields.DateTimeField(auto_now=True)
+    is_vegan = fields.BooleanField(default=False)
+
+
+class Menu(models.Model):
+    name = fields.CharField(unique=True, max_length=100)
+    description = fields.TextField()
+    date_added = fields.DateTimeField(auto_now_add=True)
+    date_modified = fields.DateTimeField(auto_now=True)
+    dishes = models.ManyToManyField(to=Dish)
+
---
+++ emenu/menu/serializers.py
@@ -0,0 +1,2 @@
+from emenu.menu.models import *
+from rest_framework import serializers
